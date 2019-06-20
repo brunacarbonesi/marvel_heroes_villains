b07165a1d8ad8dbe180422d158ddbb4456c86697
brunacarbonesi <brunacarbonesi@gmail.com>
2019-06-20 14:29:55 +0100

    Creating Character Adapter

--- app/src/main/java/com/bcarbonesi/marvelheroesvilains/adapter/CharacterAdapter.java
+++ app/src/main/java/com/bcarbonesi/marvelheroesvilains/adapter/CharacterAdapter.java
@@ -1,4 +1,46 @@
 package com.bcarbonesi.marvelheroesvilains.adapter;
 
-public class CharacterAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bcarbonesi.marvelheroesvilains.CharacterObject;
+import com.bcarbonesi.marvelheroesvilains.R;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class CharacterAdapter extends ArrayAdapter
+
+{
+
+    private Context context;
+    private List<CharacterObject> characters;
+
+    public CharacterAdapter (Context context, List<CharacterObject> list)
+    {
+        super(context, R.layout.character_list_view, list);
+        this.context = context;
+        this.characters = list;
+
+    }
+
+    @android.support.annotation.NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.character_list_view, parent, false);
+
+        TextView textViewHeroesLV = convertView.findViewById(R.id.textViewHeroesLV);
+        ImageView imageViewHeroesBackground = convertView.findViewById(R.id.imageViewHeroesBackground);
+
+        return convertView;
+    }
 }
